275757cf65264516967248c9aa6d1028e004b136
CaioJrVS <caio.junior.vs@gmail.com>
2021-02-05 11:04:17 -0300

    add button to generate chart // change resize package to splitt package

--- src/containers/ChartPlotter/ChartPlotter..js
+++ src/containers/ChartPlotter/ChartPlotter..js
@@ -4,13 +4,20 @@ import Header from '../../components/Header/Header'
 import CodeMirror from '../../components/CodeMirror/CodeMirror'
 import RenderLineChart from '../../components/RenderLineChart/RenderLineChart'
 
-class ChartPlotter extends Component {
+//Component to splitt the chart and the input data
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css'
 
-    /* 
-        On this component Im using the package Codemirror so I can 
-        display a web code edditor, it's very unopinated so almost
-        all configurations is delegated to the developer
-    */
+//Style 
+import './style.css'
+import classes from './ChartPlotter.module.css'
+
+/* 
+    This is the main container, it holds the methods used to handle data,
+    props and state
+*/
+
+class ChartPlotter extends Component {
 
     constructor(props){
         super(props)
@@ -31,13 +38,24 @@ class ChartPlotter extends Component {
         return (
             <div>
                 <Header />
-                <CodeMirror 
-                    code={this.state.codeData}
-                    getcode = {this.getCodeData}
-                />
-                <RenderLineChart />
-                <div>Graph Plot Area</div>
-                <div>Generate Chart fixed at the bottom</div>
+                <SplitterLayout
+                    primaryIndex={0}
+                    vertical={true}
+                    primaryMinSize={30}
+                    secondaryMinSize={80}
+                    customClassName="CustomSplitterLayoutStyle"
+                >
+                    <CodeMirror
+                        code={this.state.codeData}
+                        getcode={this.getCodeData}
+                    />
+                    <RenderLineChart />
+                </SplitterLayout>
+                <div className={classes.DivFixedBottom}>
+                    <button className={classes.ButtonGenerateChart}>
+                        GENERATE CHART
+                    </button>
+                </div>
             </div>
         )
     }
